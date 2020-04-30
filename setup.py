1cc8db2f48f91d41c4f6afe2d8f3a86b80d77003
palao <david.palao@gmail.com>
2020-04-30 16:10:04 +0200

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='cookiecutter-pypackage',
     packages=[],
-    version='0.3.3',
+    version='0.3.4',
     description='Cookiecutter template for a Python package',
     author='Audrey Roy Greenfeld',
     license='BSD',
